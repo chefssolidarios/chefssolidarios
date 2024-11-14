631da6ce212ed19937dea79bac1ebcd98db2bf07
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-11-14 18:31:50 -0300

    rounded herobanner

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,7 @@
 export const Header = () => {
   return (
     <div className=" w-full lg:max-w-screen-xl">
-      <div className="bg-blue-400 rounded-2xl px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="bg-blue-400 rounded-3xl px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
           <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
             <a href="/" className="mb-6 sm:mx-auto">
