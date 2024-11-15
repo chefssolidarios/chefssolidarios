9ef2ebaef0c38856d7c82fafc6951525cd06f45f
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-11-14 23:51:19 -0300

    Added new cards copys

--- src/app/components/Cards.jsx
+++ src/app/components/Cards.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 
 const CardComponent = ({ title, description, linkText, linkUrl }) => {
-
-svgs=
-Truck
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-truck"><path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2"/><path d="M15 18H9"/><path d="M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H14"/><circle cx="17" cy="18" r="2"/><circle cx="7" cy="18" r="2"/></svg>
-
-Chefs
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chef-hat"><path d="M17 21a1 1 0 0 0 1-1v-5.35c0-.457.316-.844.727-1.041a4 4 0 0 0-2.134-7.589 5 5 0 0 0-9.186 0 4 4 0 0 0-2.134 7.588c.411.198.727.585.727 1.041V20a1 1 0 0 0 1 1Z"/><path d="M6 17h12"/></svg>
-
-volunteer
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-handshake"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/><path d="M12 5 9.04 7.96a2.17 2.17 0 0 0 0 3.08c.82.82 2.13.85 3 .07l2.07-1.9a2.82 2.82 0 0 1 3.79 0l2.96 2.66"/><path d="m18 15-2-2"/><path d="m15 18-2-2"/></svg>
-
-dame otra idea del relleno
-
+  const icons = [
+    {
+      name: "Truck",
+      svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-truck"><path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2"/><path d="M15 18H9"/><path d="M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H14"/><circle cx="17" cy="18" r="2"/><circle cx="7" cy="18" r="2"/></svg>`,
+    },
+    {
+      name: "Chefs",
+      svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chef-hat"><path d="M17 21a1 1 0 0 0 1-1v-5.35c0-.457.316-.844.727-1.041a4 4 0 0 0-2.134-7.589 5 5 0 0 0-9.186 0 4 4 0 0 0-2.134 7.588c.411.198.727.585.727 1.041V20a1 1 0 0 0 1 1Z"/><path d="M6 17h12"/></svg>`,
+    },
+    {
+      name: "Volunteer",
+      svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-handshake"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/><path d="M12 5 9.04 7.96a2.17 2.17 0 0 0 0 3.08c.82.82 2.13.85 3 .07l2.07-1.9a2.82 2.82 0 0 1 3.79 0l2.96 2.66"/><path d="m18 15-2-2"/><path d="m15 18-2-2"/></svg>`,
+    },
+    {
+      name: "Idea",
+      svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lightbulb"><path d="M9 18h6"/><path d="M10 22h4"/><path d="M4 10a8 8 0 1 1 16 0c0 3-1.5 5-3 6a8 8 0 0 1-10 0c-1.5-1-3-3-3-6Z"/></svg>`,
+    },
+  ];
 
   return (
     <div className="relative p-px overflow-hidden transition duration-300 transform border rounded shadow-sm hover:scale-105 group hover:shadow-xl">
--- src/app/particular/page.js
+++ src/app/particular/page.js
@@ -7,27 +7,27 @@ function Particular() {
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-0 lg:py-20">
       <div className="grid gap-8 row-gap-5 md:grid-cols-2 bg-orange-300">
         <CardComponent
-          title="Voluntario"
-          description="Quiero ser voluntario y ser parte del equipo"
-          linkText="Unirme como voluntario"
+          title="Únete como Voluntario"
+          description="Sé parte de nuestra comunidad solidaria apoyando en la cocina, reparto, marketing, gestión de stock y más."
+          linkText="Quiero ser voluntario"
           linkUrl="/"
         />
         <CardComponent
-          title="Chefs"
-          description="Quiero ser voluntario y ser parte del equipo"
-          linkText="Unirme como voluntario"
+          title="Sé un Chef Solidario"
+          description="Usa tu talento culinario para preparar comidas para quienes más lo necesitan"
+          linkText="Quiero ser un Chef Solidario"
           linkUrl="/"
         />
         <CardComponent
-          title="Reparto"
-          description="'¿Tienes vehículo y quieres ayudarnos a entregar la comida calentita?"
-          linkText="Unirme como voluntario"
+          title="Ayuda con el Reparto"
+          description="¿Tienes vehículo? únete para entregar comidas calientes a las familias afectadas."
+          linkText="Quiero ayudar con el reparto"
           linkUrl="/"
         />
         <CardComponent
-          title="Relleno"
-          description="Aca tiene que ir algo de relleno"
-          linkText="Unirme como voluntario"
+          title="Apoya con Tu Donación"
+          description="Con tu contribución, podremos seguir cocinando y entregando comidas a quienes más lo necesitan. ¡Cada donación cuenta!"
+          linkText="Quiero hacer una donación"
           linkUrl="/"
         />
       </div>
