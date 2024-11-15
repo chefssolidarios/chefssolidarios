f11d29821a4d25541b24743253858b6be573486c
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-11-14 21:17:01 -0300

    Updating banner style

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,22 +3,17 @@
 export const Header = () => {
   return (
     <div className=" w-full lg:max-w-screen-xl z-10">
-      <div className="bg-gradient-to-tr from-chefs-800 to-chefs-400 rounded-3xl px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
-          <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
-            <a href="/" className="mb-6 sm:mx-auto">
-              <div className="flex items-center justify-center w-12 h-12 rounded-full bg-teal-accent-400"></div>
-            </a>
-            <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white sm:text-4xl md:mx-auto">
-                <span className="relative inline-block">
-                  <span className="relative">The</span>
-                </span>{" "}
-                quick, brown fox jumps over a lazy dog
+      <div className="bg-gradient-to-tr from-chefs-800 to-chefs-400 rounded-3xl px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-28 lg:pb-12">
+        <div className=" sm:mx-auto  bg-orange-300">
+          <div className="flex flex-col md:flex-row mb-16 sm:text-center sm:mb-0 items-end justify-between">
+            <div className=" bg-green-400 max-w-3xl text-left">
+              <h2 className=" mb-2 font-sans text-3xl font-extrabold tracking-tight text-white sm:text-4xl md:mx-auto md:text-9xl ">
+                CHEFS <br /> SOLIDARIOS
               </h2>
               <p className="text-base text-indigo-100 md:text-lg">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque rem aperiam, eaque ipsa quae.
+                Juntos, cocinamos y distribuimos alimentos calientes para los
+                afectados por la DANA en Valencia y alrededores. Tu ayuda marca
+                la diferencia.
               </p>
             </div>
             <div>
